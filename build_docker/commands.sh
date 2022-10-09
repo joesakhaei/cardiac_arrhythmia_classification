b4306e08ff74671795c2f446cd96818bd048dc62
joesakhaei <115425045+joesakhaei@users.noreply.github.com>
2022-10-10 07:00:29 +1100

    Fixed
    Fixed

--- build_docker/commands.sh
+++ build_docker/commands.sh
@@ -3,5 +3,3 @@
 spark-submit --master local --name app_train --executor-memory 5g --driver-memory 5g /usr/local/train.py
 
 spark-submit --master local --name app_inference --executor-memory 4g --driver-memory 4g /usr/local/inference.py
-
-jupyter lab /usr/local/
